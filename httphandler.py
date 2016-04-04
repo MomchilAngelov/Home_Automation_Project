015ff7691fc41534bbf9944eac80e56bef163f1d
3phase <pbozhinov@yahoo.com>
2016-04-04 18:28:30 +0300

    Had problem processing request, now the problem is partly solved; handler class was previously shunned

--- httphandler.py
+++ httphandler.py
@@ -2,24 +2,26 @@ import http.server
 import socketserver
 import sys
 
+REDIRECTIONS = {"/slashdot/": "http://slashdot.org/", "/freshmeat/": "http://freshmeat.net/"}
+
 class HttpHandler(http.server.BaseHTTPRequestHandler):
 
-	def __init__(self, host, port):
-		self.PORT = int(port)
-		self.Handler = http.server.SimpleHTTPRequestHandler
-		self.httpd = None
-
-	def run(self):
-		self.httpd = socketserver.TCPServer(("", self.PORT), self.Handler)
-		print("serving at port", self.PORT)
-		self.httpd.serve_forever()
-
 	def do_GET(self):
-		if self.path == "/":
-			self.path="/index.html"
+
+		self.send_response(200)
+		# Send headers
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+
+		# Send message back to client
+		# if self.path=="/":
+		self.send_header("Location", REDIRECTIONS.get(self.path, "/index.html"))
+		# self.path="/index.html"
 
 		if self.path == "/configure":
 			self.path = "/init_configuration.html"
+
+		return
 
 
 	def do_POST(self):
@@ -34,5 +36,13 @@ class HttpHandler(http.server.BaseHTTPRequestHandler):
 			self.end_headers()
 
 
-h = HttpHandler("localhost", sys.argv[1])
-h.run()
+
+PORT = sys.argv[1]
+httpd = http.server.HTTPServer(("95.111.79.35", int(PORT)), HttpHandler)
+print("serving at port", PORT)
+
+try:
+	httpd.serve_forever()
+except KeyboardInterrupt as e:
+	pass
+
