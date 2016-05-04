41e8a02ee02052a8ae03ee84110f5c6a3120e311
3phase <pbozhinow@gmail.com>
2016-05-04 08:20:19 +0000

    Interface updates

--- distance_measure.py
+++ distance_measure.py
@@ -7,13 +7,19 @@ TRIG = 11
 ECHO = 16
 TRIG2 = 15
 ECHO2 = 13
+left_to_right = 0
+right_to_left = 0
+counter = 0
+number_of_tries = 10
 
-def get_value():
+def get_value_distance():
 	global TRIG
 	global ECHO
 	global TRIG2
 	global ECHO2
-
+	global left_to_right
+	global right_to_left
+	global counter
 
 	GPIO.output(TRIG, True)
 	time.sleep(0.00001)
@@ -25,8 +31,7 @@ def get_value():
 		pulse_end = time.time()
 	pulse_duration = pulse_end - pulse_start
 	distance = pulse_duration * 17150
-	distance = round(distance, 2)
-
+	distance = int(distance)
 
 	GPIO.output(TRIG2, True)
 	time.sleep(0.00001)
@@ -40,7 +45,22 @@ def get_value():
 
 	pulse_duration2 = pulse_end2 - pulse_start2
 	distance2 = pulse_duration2 * 17150
-	distance2 = round(distance2, 2)
+	distance2 = int(distance2)
+
+	if distance2 < 10 and distance > 10 and right_to_left == 0:
+		print("Moving left to right!(out)")
+		left_to_right = 1
+		counter = 0
+	if distance < 10 and distance2 > 10 and left_to_right == 0:
+		print("Moving right to left! (in)")
+		right_to_left = 1
+		counter = 0
+	if distance > 10 and distance2 > 10:
+		counter += 1
+		if counter > 3:
+			left_to_right = 0
+			right_to_left = 0
+
 	return distance, distance2
 
 
@@ -54,10 +74,11 @@ GPIO.output(TRIG2, False)
 print("Waiting to settle!")
 i = 0
 while True:
-	time.sleep(2)
-	distance_from_one, distance_from_two = get_value()
+	time.sleep(1)
+	distance_from_one, distance_from_two = get_value_distance()
 	print("Distance from one: {0}cm;\nDistance from two: {1}cm;\n".format(distance_from_one, distance_from_two))
 	i += 1
-	if i == 5:
-		break
+	if i == number_of_tries:
+		#break
+		pass
 GPIO.cleanup()
